609caaad278ad515b59acefcd6c2a52a8af3a925
hana Charaabi <charaabihana90@gmail.com>
2019-05-23 10:44:25 +0100

    Updates

---
+++ precache-manifest.387e3d4196bdb0cc088af361acef2ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe308ec089ff59274a0078d0f609cbb",
+    "url": "/movie-react-app-search/index.html"
+  },
+  {
+    "revision": "963bb25193e996be01f2",
+    "url": "/movie-react-app-search/static/css/main.6dc1021e.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/movie-react-app-search/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "963bb25193e996be01f2",
+    "url": "/movie-react-app-search/static/js/main.f47c131e.chunk.js"
+  },
+  {
+    "revision": "72b364a492781cd1a346",
+    "url": "/movie-react-app-search/static/js/runtime~main.46993785.js"
+  }
+]);
